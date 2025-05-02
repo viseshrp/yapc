38d0dfd5aa6b164e5c2f61045575cfaa16f99969
viseshrp <viseshrprasad@gmail.com>
2025-05-01 22:03:46 -0400

    fix versioning

--- {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/_version.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/_version.py
@@ -1,1 +1,7 @@
-__version__ = "{{ cookiecutter.project_version }}"
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("{{ cookiecutter.project_name }}")
+except PackageNotFoundError:  # pragma: no cover
+    # Fallback for local dev or editable installs
+    __version__ = "0.0.0"
