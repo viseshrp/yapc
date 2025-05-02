37b0ebebfdc7ac090886b10451f7595d7ba2bc9d
viseshrp <viseshrprasad@gmail.com>
2025-05-02 17:55:13 -0400

    fix versioning

--- {{cookiecutter.project_name}}/scripts/version.py
+++ {{cookiecutter.project_name}}/scripts/version.py
@@ -6,7 +6,11 @@ from hatch_vcs.version import VCSVersion
 
 def version_scheme(version: VCSVersion) -> str:
     """Generate a version like 0.1.1.dev20250430173015"""
-    base_version = version.tag.base_version if version.tag else "0.0.0"
+    try:
+        base_version = version.tag.base_version if version.tag else "0.0.0"
+    except Exception:
+        base_version = "0.0.0"
+
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     return f"{base_version}.dev{timestamp}"
 
