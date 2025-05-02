506a96985c0515017bd7fb975fef436b4ca2cad3
viseshrp <viseshrprasad@gmail.com>
2025-05-02 16:55:49 -0400

    Update version.py

--- {{cookiecutter.project_name}}/scripts/version.py
+++ {{cookiecutter.project_name}}/scripts/version.py
@@ -1,13 +1,16 @@
+from __future__ import annotations
+
 from datetime import datetime
+from hatch_vcs.version import VCSVersion
 
 
-def version_scheme(version):
+def version_scheme(version: VCSVersion) -> str:
     """Generate a version like 0.1.1.dev20250430173015"""
     base_version = version.tag.base_version if version.tag else "0.0.0"
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")  # no 'T'
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     return f"{base_version}.dev{timestamp}"
 
 
-def local_scheme(version):
+def local_scheme(version: VCSVersion) -> str:
     """Disable local versioning (e.g., +gabcdef)"""
     return ""
